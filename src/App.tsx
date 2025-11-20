f3053ca00af0e45826d7f45a91c754d630384474
Petro <petrotymchuck@gmail.com>
2025-11-20 07:35:52 +0200

    Add register page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Route, Routes} from "react-router";
 import MainLayout from "./layout/Main/MainLayout.tsx";
 import NotFoundPage from "./pages/common/NotFoundPage.tsx";
 import UserHomePage from "./pages/user/UserHomePage";
+import RegisterPage from "./pages/account/RegisterPage";
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
         <Routes>
             <Route path="/" element={<MainLayout />}>
                 <Route index element={<UserHomePage/>} />
+
+                <Route path={"/register"} element={<RegisterPage/>} />
             </Route>
 
             <Route path="*" element={<NotFoundPage/>} />
